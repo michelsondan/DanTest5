ab0dcb0138cbedd59dda70a67e91e55f1abfc745
michelsondan <michelson.dan@gmail.com>
2016-11-24 17:47:06 +0200

    lknjn

---
+++ geoService.ashx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DanTest5
+{
+    /// <summary>
+    /// Summary description for geoService
+    /// </summary>
+    public class geoService : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            context.Response.Write("Hello World");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
