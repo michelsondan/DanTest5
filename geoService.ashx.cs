6eed907718b133f4850212f14e02d0086af90a08
michelsondan <michelson.dan@gmail.com>
2016-11-28 18:11:55 +0200

    service

--- geoService.ashx.cs
+++ geoService.ashx.cs
@@ -13,6 +13,23 @@ namespace DanTest5
 
         public void ProcessRequest(HttpContext context)
         {
+            int userCounts;
+            if (context.Application["userCounts"] == null)
+                userCounts = 0;
+            else
+                userCounts = (int)context.Application["userCounts"];
+
+
+            if (context.Application[context.Request["user"]] == null)
+            {
+                userCounts++;
+                context.Application[context.Request["user"]] = "{lat=
+                context.Application[context.Request["user"] + "_lat"] = context.Request["lat"];
+                context.Application[context.Request["user"] + "_lon"] = context.Request["lon"];
+            }
+
+            context.Application["userCounts"] = userCounts;
+
             context.Response.ContentType = "text/plain";
             context.Response.Write("Hello World");
         }
